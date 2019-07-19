56cecdeae1d472994172af0575d8cb1ab6ed8f89
MallikKhilji <51685281+MallikKhilji@users.noreply.github.com>
2019-07-18 20:09:46 -0400

    sxa

---
+++ FIRSTPROJECT/src/basics/HelloWorld.java
@@ -0,0 +1,15 @@
+package basics;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int age = 21;
+        String name = "Malik KHILJI";
+        String address = "101 STATEN ISLAND AVENUE ";
+        System.out.println(name + " " + "is" + " " + age + " " + "lives in" + " " +  address );
+        //
+
+    }
+
+    }
